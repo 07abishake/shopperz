54f0755fb0c5ca6935d78862ef27d185c927437a
Abishake <143249013+07abishake@users.noreply.github.com>
2024-02-04 21:16:15 -0800

    Add files via upload

---
+++ src/my.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.querySelector(".addtocart");
+    const done = document.querySelector(".done");
+    console.log(button);
+    let added = false;
+
+    button.addEventListener("click", () => {
+        if (added) {
+            done.style.transform = "translate(-110%) skew(-40deg)";
+            added = false;
+        } else {
+            done.style.transform = "translate(0px)";
+            added = true;
+        }
+    });
+});
+console.log(button)
